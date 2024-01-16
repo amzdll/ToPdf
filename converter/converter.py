658522721ce245d17706dc9856d36740d4237032
glenpoin <glenpoin@student.21-school.ru>
2024-01-16 23:47:04 +0300

    init project

---
+++ converter/converter.py
@@ -0,0 +1,7 @@
+# Converter for DOC, DOCX, XLS, XLSX, PPT, PPTX, PNG, JPG, JPEG, BMP, EPS, GIF, TXT, RTF, HTML files to PDF
+import PyPDF2
+
+
+class Converter:
+    def __init__(self):
+        pass
