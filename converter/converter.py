090802c95447d26179cbf90ad63a090d698c5738
glenpoin <glenpoin@student.21-school.ru>
2024-01-21 00:36:44 +0300

    add bg to image, create method for creation a blank page A4

--- converter/converter.py
+++ converter/converter.py
@@ -1,12 +1,9 @@
-import base64
+import tempfile
 from io import BytesIO
-import io
 import pathlib
 import PyPDF2
-from fpdf import FPDF
 from PIL import Image
-from typing import Callable
-import docx2pdf
+from typing import Callable, BinaryIO
 
 
 # Converter for DOC, DOCX, XLS, XLSX, PPT, PPTX, EPS, GIF, TXT, RTF, HTML files to PDF
@@ -30,14 +27,27 @@ class Converter:
         }
 
     # Methods for convert in_file to pdf
+
     @staticmethod
-    def __img_to_pdf(source_data, result_data: BytesIO) -> None:
+    def __change_image_size(image_size: tuple) -> tuple[int, int]:
+        return ((image_size[0] / 495) * image_size
+                if image_size[0] >= image_size[1]
+                else (image_size[0] / 742) * image_size)
+
+    def __img_to_pdf(self, source_data, result_data: BytesIO) -> None:
         with Image.open(source_data) as image:
             if image.mode == "RGBA":
                 image.convert("RGB")
-            image.save(result_data, format="PDF")
 
-    #   fix
+            if image.size[0] > 495 or image.size[1] > 742:
+                image.resize(self.__change_image_size(image.size))
+
+            background = Image.new(mode="RGB", size=(595, 842), color="white")
+            place: tuple[int, int] = (int((background.size[0] - image.size[0]) / 2),
+                                      int((background.size[1] - image.size[1]) / 2))
+            background.paste(image, place)
+            background.save(result_data, format="PDF")
+
     @staticmethod
     def __gif_to_pdf(source_data, result_data: BytesIO) -> None:
         with Image.open(source_data) as gif:
@@ -77,18 +87,19 @@ class Converter:
     #  fix: empty pdf, data is clear
     @staticmethod
     def __txt_to_pdf(source_data: BytesIO, result_data: BytesIO) -> BytesIO:
-        pdf = FPDF()
-        pdf.add_page()
-        pdf.set_font("Arial", size=14)
-        pdf.write(5, source_data.read().decode("utf-8"))
-        pdf_output = pdf.output(dest='S').encode('utf-8')
-        result_data.seek(0)
-        result_data.write(pdf_output)
-        result_data.seek(0)
-        with open("result.pdf", "wb") as result_file:
-            result_file.write(result_data.getvalue())
-
-        return result_data
+        return BytesIO()
+        # pdf = FPDF()
+        # pdf.add_page()
+        # pdf.set_font("Arial", size=14)
+        # pdf.write(5, source_data.read().decode("utf-8"))
+        # pdf_output = pdf.output(dest='S').encode('utf-8')
+        # result_data.seek(0)
+        # result_data.write(pdf_output)
+        # result_data.seek(0)
+        # with open("result.pdf", "wb") as result_file:
+        #     result_file.write(result_data.getvalue())
+        #
+        # return result_data
 
         # pdf = FPDF()
         # pdf.add_page()
@@ -115,21 +126,36 @@ class Converter:
             return file_type
         elif file_type in self.__imgs_formats:
             return "img"
+        else:
+            return ""
+
+    def __create_temporary_pdf(self):
+        temporary_file = tempfile.NamedTemporaryFile(mode="wb", suffix="pdf")
+        page = PyPDF2.PageObject.create_blank_page(width=595.276, height=841.891)
+        self.__pdf_writer.add_page(page)
+        with open(temporary_file.name, "wb") as f:
+            self.__pdf_writer.write(f)
+        return temporary_file
 
     def __add_page(self, file) -> None:
         self.__pdf_reader = PyPDF2.PdfReader(file)
         page_count = len(self.__pdf_reader.pages)
         print(page_count)
 
-    def convert(self, file_name: str, source_data) -> BytesIO:
+    def convert(self, file_name: str, source_data: BinaryIO) -> BytesIO:
         result_data = BytesIO()
         self.__conversion_methods[self.__extract_filetype(file_name)](source_data, result_data)
         return result_data
 
 
-if __name__ == "__main__":
-    converter = Converter()
+converter = Converter()
+path = "/home/freiqq/Projects/Python/ToPdf/tests/resources/test.jpeg"
+with open(path, "rb") as f:
+    converter.convert("test.jpeg", f)
 
-    path = "/Users/glenpoin/W/Projects/Python/ToPdf/tests/resources/test.txt"
-    f = open(path, "rb")
-    s_data = converter.convert(path, f)
+# if __name__ == "__main__":
+#     converter = Converter()
+#
+#     path = "/Users/glenpoin/W/Projects/Python/ToPdf/tests/resources/test.txt"
+#     f = open(path, "rb")
+#     s_data = converter.convert(path, f)
