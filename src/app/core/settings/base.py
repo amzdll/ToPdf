601a2021c03379e1b0533acbd1ebb708c1aa442f
glenpoin <glenpoin@student.21-school.ru>
2024-02-04 04:40:01 +0300

    fix db urls

--- src/app/core/settings/base.py
+++ src/app/core/settings/base.py
@@ -20,14 +20,19 @@ class BaseAppSettings(BaseSettings):
 
     @property
     def database_url_asyncpg(self) -> str:
-        return f"postgresql+asyncpg://\
-        {self.db_user}:{self.db_pass}@\
-        {self.db_host}:{self.db_port}/{self.db_name}"
+        return (
+            f"postgresql+asyncpg://{self.db_user}:"
+            f"{self.db_pass}@{self.db_host}:"
+            f"{self.db_port}/{self.db_name}"
+        )
 
     @property
     def database_url(self) -> str:
-        return f"postgresql://\
-        {self.db_user}:{self.db_pass}@{self.db_host}/{self.db_name}"
+        return (
+            f"postgresql://"
+            f"{self.db_user}:{self.db_pass}@"
+            f"{self.db_host}/{self.db_name}"
+        )
 
     class Config:
         env_file = ".env"
