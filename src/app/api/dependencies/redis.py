8ba11763268fd6119b127cb4a2320286e5981506
amzdll <frei3453@gmail.com>
2024-02-05 20:45:43 +0300

    first attempts

--- src/app/api/dependencies/redis.py
+++ src/app/api/dependencies/redis.py
@@ -1,17 +1,7 @@
-from functools import lru_cache
-
 import aioredis
-from fastapi import Depends
-from src.app.core.config import get_app_settings
-
-settings = get_app_settings()
+from aioredis import Redis
 
 
-@lru_cache
-async def get_redis():
-    redis = await aioredis.from_url(settings.redis_url)
+async def get_redis(settings):
+    redis = aioredis.from_url(settings.redis_url, decode_responses=True)
     return redis
-
-
-async def get_cached_settings(redis: aioredis.Redis = Depends(get_redis)):
-    return await redis.get("settings")
